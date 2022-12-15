f289f99eca651d40b8482cf3a8cc39e42fffc8ea
abhimanyu kumar <abhimanyukumar24038@gmail.com>
2022-12-15 23:12:56 +0530

    #	modified:   src/pages/AddNewUser.jsx

--- src/pages/AddNewUser.jsx
+++ src/pages/AddNewUser.jsx
@@ -1,7 +1,54 @@
+import React from "react";
+
 const AddNewUser = ()=>{
-    return <div className="title">
-         <h2>abhimanyu coder</h2>
-    </div>
-};
+
+    const ClickHandler =   ()=>{
+
+    }
+    return ( <div className="wrapper rounded bg-white">
+                <div className="h3">Add New User</div>
+                    <div className="form">
+                      <div className="row">
+                         <div className="col-value">
+                           <label>Name</label>
+                           <input type="text" className="form-control"  required/>
+                         </div>
+                         <div className="col-value">
+                           <label>Roll Number</label>
+                           <input type="text" className="form-control"  required/>
+                         </div>
+                         <div className="col-value">
+                           <label>Mobile Number</label>
+                           <input type="text" className="form-control" required/>
+                         </div>
+                         <div className="col-value">
+                           <label>Email</label>
+                           <input type="text" className="form-control"  required/>
+                         </div>                   
+                         <div className="col-value">
+                           <label>Branch</label>
+                           <input type="text" className="form-control"  required/>
+                         </div>                    
+                         <div className="col-value">
+                            <label>Session</label>
+                            <input type="text" className="form-control" required/>
+                         </div>
+                         <div className="col-value">
+                           <label>Hostel</label>
+                           <input type="text" className="form-control" required/>
+                         </div>
+                         <div className="col-value">
+                           <label>Room Number</label>
+                           <input type="text" className="form-control" required/>
+                         </div>                   
+                         <div className="col-value">
+                           <label>Role</label>
+                           <input type="text" className="form-control" required/>
+                         </div>
+                         </div>
+                    <button className="btn btn-primary mt-3" onClick={ClickHandler}>Submit           </button>
+                </div>
+             </div>
+)};
 
 export default AddNewUser;
